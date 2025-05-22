bb165302afddc893a5cd75591fc8ab6415a61ee1
Eric Joyner <erj@erj.cc>
2025-05-21 18:13:31 -0700

    Initial commit
    Add enough CMake and C++ code to get a basic .exe compiling on Windows

---
+++ app/main.cc
@@ -0,0 +1,15 @@
+/**
+ * Test application for building and checking includes
+ */
+
+#include <Windows.h>
+#include <cstring>
+
+#include <libheif/heif.h>
+
+int main(int argc, char** argv)
+{
+    heif_context *ctx = heif_context_alloc();
+
+    return 0;
+}
