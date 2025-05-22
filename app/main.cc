a39046e9f7727ac6470591a1975646fc773bb2bd
Eric Joyner <erj@erj.cc>
2025-05-21 19:52:47 -0700

    Improve build
    Improve libheif compilation by adding variables to include libde265

--- app/main.cc
+++ app/main.cc
@@ -4,12 +4,30 @@
 
 #include <Windows.h>
 #include <cstring>
+#include <iostream>
 
 #include <libheif/heif.h>
 
+// Used from libheif's examples/heif_dec.cc
+class LibHeifInitializer {
+public:
+    LibHeifInitializer() { heif_init(nullptr); }
+    ~LibHeifInitializer() { heif_deinit(); }
+};
+
 int main(int argc, char** argv)
 {
-    heif_context *ctx = heif_context_alloc();
+    // automatically inits and deinits the library in main() scope
+    LibHeifInitializer initializer;
+
+    heif_context* ctx = heif_context_alloc();
+
+    if (ctx)
+        printf("HEIF Context Allocated!\n");
+    else
+        printf("HEIF Context Allocation failed.\n");
+
+    heif_context_free(ctx);
 
     return 0;
 }
