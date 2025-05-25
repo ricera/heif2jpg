066c3088ef70c98aa3010a61393a8080b19c9ab9
Eric Joyner <erj@erj.cc>
2025-05-25 13:10:20 -0700

    Use shim FindJPEG.cmake for libultrahdr
    This lets libultrahdr use the libjpegturbo compiled by the main project and finally compile successfully.

---
+++ cmake/modules/FindJPEG.cmake
@@ -0,0 +1,12 @@
+#include(LibFindMacros)
+#libfind_pkg_check_modules(JPEG_PKGCONF libjpegturbo)
+
+message(STATUS "FindJPEG -- Looking for JPEG vars: ${JPEG_MODULE_PATH}")
+
+set(JPEG_INCLUDE_DIRS ${JPEG_MODULE_PATH}/include)
+set(JPEG_LIBRARIES ${JPEG_MODULE_PATH}/lib/jpeg-static.lib)
+
+message(STATUS "FindJPEG var JPEG_INCLUDE_DIRS: ${JPEG_INCLUDE_DIRS}")
+message(STATUS "FindJPEG var JPEG_LIBRARIES: ${JPEG_LIBRARIES}")
+
+set(JPEG_FOUND true)
